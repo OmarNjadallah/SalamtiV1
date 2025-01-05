5c1aa8aab6ec4291c8a759276ed1d63b1974030a
OmarNjadallah <112210593+OmarNjadallah@users.noreply.github.com>
2025-01-06 02:50:32 +0300

    1234

--- SalamtiWebApp/Config/GoogleAPIkey.js
+++ SalamtiWebApp/Config/GoogleAPIkey.js
@@ -4,7 +4,7 @@ import {
   fireBaseAuthDomain,
   fireBaseProjectId,
 } from "./APIKEYS";
-export const googleApiKey = "AIzaSyBKs_fbiolpGriQHbb0-x8z68Y0p-b2OrY";
-export const fireBaseApiKey = "AIzaSyDAkCAc9ix8RUZ4zigg6bXWkBctwiEXtCE";
-export const fireBaseAuthDomain = "salamti-30fd8.firebaseapp.com";
-export const fireBaseProjectId = "salamti-30fd8";
+export const googleApiKey = googleApiKey;
+export const fireBaseApiKey = fireBaseApiKey;
+export const fireBaseAuthDomain = fireBaseAuthDomain;
+export const fireBaseProjectId = fireBaseProjectId;
