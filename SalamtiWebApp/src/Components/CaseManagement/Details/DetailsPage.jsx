3277c9bd87ae901c8b1e5734052a7d53fdef4681
OmarNjadallah <112210593+OmarNjadallah@users.noreply.github.com>
2025-01-06 01:32:18 +0300

    change border of navbar to black, and background colors of details page cards to black

--- SalamtiWebApp/src/Components/CaseManagement/Details/DetailsPage.jsx
+++ SalamtiWebApp/src/Components/CaseManagement/Details/DetailsPage.jsx
@@ -16,22 +16,24 @@ const ViewCasePage = ({ caseDetails }) => {
 
   return (
     <div className="bg-black text-white shadow-lg rounded-lg max-w-full w-full p-4 lg:p-8 mx-auto space-y-8 h-screen overflow-scroll">
-      <h1 className="text-3xl font-bold text-center lg:text-left">Case Details</h1>
+      <h1 className="text-3xl font-bold text-center lg:text-left">
+        Case Details
+      </h1>
 
       {/* First Section: Case Info, Map, and Emergency Details */}
       <div className="flex flex-col lg:flex-row lg:gap-6">
         {/* Case Information Table */}
-        <div className="flex-1 bg-gray-900 p-4 rounded-md shadow-md">
+        <div className="flex-1 bg-black p-4 rounded-md shadow-md">
           <DetailsTable caseDetails={caseDetails} />
         </div>
 
         {/* Location Map */}
-        <div className="flex-1 bg-gray-900 p-4 rounded-md shadow-md mt-6 lg:mt-0">
+        <div className="flex-1 bg-black p-4 rounded-md shadow-md mt-6 lg:mt-0">
           <DetailsMap caseDetails={caseDetails} />
         </div>
 
         {/* Emergency Details */}
-        <div className="flex-1 bg-gray-900 p-4 rounded-md shadow-md mt-6 lg:mt-0">
+        <div className="flex-1 bg-black p-4 rounded-md shadow-md mt-6 lg:mt-0">
           <DetailsEmergencyDetails caseDetails={caseDetails} />
         </div>
       </div>
@@ -39,12 +41,12 @@ const ViewCasePage = ({ caseDetails }) => {
       {/* Second Section: Arrival and Finish Times */}
       <div className="flex flex-col lg:flex-row lg:gap-6">
         {/* ESP Arrival Times Table */}
-        <div className="flex-1 bg-gray-900 p-4 rounded-md shadow-md overflow-scroll">
+        <div className="flex-1 bg-black p-4 rounded-md shadow-md overflow-scroll">
           <DetailsArrivalTimes caseDetails={caseDetails} />
         </div>
 
         {/* ESP Finish Times Table */}
-        <div className="flex-1 bg-gray-900 p-4 rounded-md shadow-md overflow-scroll">
+        <div className="flex-1 bg-black p-4 rounded-md shadow-md overflow-scroll">
           <DetailsFinishTimes caseDetails={caseDetails} />
         </div>
       </div>
