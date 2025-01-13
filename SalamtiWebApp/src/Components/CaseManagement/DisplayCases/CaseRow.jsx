4984e9b8bdba989c834277c48f6f099148e9fb11
OmarNjadallah <112210593+OmarNjadallah@users.noreply.github.com>
2025-01-13 10:51:40 +0300

    minor css changes

--- SalamtiWebApp/src/Components/CaseManagement/DisplayCases/CaseRow.jsx
+++ SalamtiWebApp/src/Components/CaseManagement/DisplayCases/CaseRow.jsx
@@ -9,6 +9,8 @@ const CaseRow = ({ caseData }) => {
       ? "bg-yellow-100 text-yellow-800 font-semibold"
       : caseData.Status === "complete"
       ? "bg-green-100 text-green-800 font-semibold"
+      : caseData.Status === "cancelled"
+      ? "bg-red-100 text-red-800 font-semibold"
       : "";
 
   return (
